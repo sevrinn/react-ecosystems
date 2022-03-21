60da288eeeaeb8c5e69c765062b591e75b657163
Sevrinn <sevrinnsevrinn@gmail.com>
2022-03-21 13:36:31 -0700

    import todo list form

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import TodoListItem from './TodoListItem'
+import NewTodoForm from './NewTodoForm'
 import './TodoList.css'
 
 //TodoList takes todos as a prop and maps out each todo
-const TodoList = ({ todos }) => (
+const TodoList = ({ todos = [] }) => (
 	<div className='list-wrapper'>
+		<NewTodoForm />
 		{todos.map((todo) => (
 			<TodoListItem todo={todo} />
 		))}
