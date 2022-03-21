7a0c424c65f1f01faf5cc266c53c5849c906ac5e
Sevrinn <sevrinnsevrinn@gmail.com>
2022-03-21 13:15:10 -0700

    TodoListItem component

---
+++ src/todos/TodoListItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TodoListItem = ({ todo }) => (
+	<div className='todo-item-container'>
+		<h3>{todo.text}</h3>
+		<div className='btn-container'>
+			<button className='completed-btn'>Mark As Completed</button>
+			<button className='remove-btn'>Remove</button>
+		</div>
+	</div>
+)
+
+export default TodoListItem
